9057f368d18f769c5689e3b81bd93c919ab6b721
Andy R <xnorlogic@gmail.com>
2019-03-05 15:42:05 -0500

    Update application.c
    space optimization

--- Application/application.c
+++ Application/application.c
@@ -52,14 +52,11 @@ void TestDotDisp(void){
     /*Display the character 1 at the beginning of line 5 of the screen*/
     StartLine(5U);
     WriteChar('1', 0U);
-
     /*display 2 at the beginning of line 9*/
     StartLine(9U);
     WriteChar('2', 0U);
-
     /*write 10 1's at line 3*/
     for(i = 0 ;i<10;i++){
         WriteCharAt('1',i,3U,0U);
     }
-
 }
